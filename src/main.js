2d2f60e5dafceb63eb255aac2166829e11f5d956
Vitalii <ilovepinkpony1@gmail.com>
2019-05-26 17:45:07 +0300

    solution

--- src/main.js
+++ src/main.js
@@ -1,17 +1,15 @@
 'use strict';
 
-window.addEventListener('load', main);
-
-function main() {
-  selectImage();
-}
+window.addEventListener('load', selectImage);
 
 function selectImage() {
   const mainImg = document.getElementById('mainImg');
   const smallImages = document.querySelector('.row-img');
   
-  smallImages.addEventListener('click', () =>{
-  mainImg.src = event.target.src;
-  mainImg.alt = event.target.alt;
+  smallImages.addEventListener('click', () => {
+    if (event.target.src) {
+      mainImg.src = event.target.src;
+      mainImg.alt = event.target.alt;
+    }
   });
 }
